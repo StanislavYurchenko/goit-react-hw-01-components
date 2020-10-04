3ec6cc762efcd4f5d0ecafc9de07bc3b3db9bd24
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-10-04 21:00:58 +0300

    Updates

---
+++ precache-manifest.f328d490dccbb0bfa3f27ce9cf908849.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7772340aba0a9eff1262cca5694a6331",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "5d509427885e9216c2df",
+    "url": "/goit-react-hw-01-components/static/js/2.ab3b7fc0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-01-components/static/js/2.ab3b7fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3596e6298ac2694be15a",
+    "url": "/goit-react-hw-01-components/static/js/main.acfebeaa.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
